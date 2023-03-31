7584be54497a383c52162f2d53221b1b789eb5c2
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-03-31 01:13:28 -0300

    Added script for selecting products names
    + Button color change

--- scripts/general.js
+++ scripts/general.js
@@ -1,6 +1,13 @@
+let selectedProducts = [null, null, null];
+let productsType = [];
+
+const main = document.querySelector(".main");
+for (let i = 0; i < main.children.length; i++) {
+    productsType.push(main.children[i].className);  
+} 
 
 function chooseProduct(selector) {
-    
+
     let lastChosenProduct = null;
     for (let i = 0; i < selector.parentElement.children.length; i++) {
 
@@ -14,4 +21,23 @@ function chooseProduct(selector) {
         lastChosenProduct.classList.remove("chosen-product");
     }
     selector.classList.add("chosen-product");
+    insertProductArray(selector.parentElement.parentElement.className, selector);
+}
+
+function insertProductArray(productType, selector) {
+
+    const productIndex = productsType.indexOf(productType);
+    selectedProducts[productIndex] = selector.children[1].innerText;
+    console.log(selectedProducts);
+    checkBtn();
+}
+
+function checkBtn() {
+
+    if (!selectedProducts.includes(null)) {
+
+       const orderBtn = document.querySelector(".order-button");
+       orderBtn.classList.add("finished-order");
+       orderBtn.children[0].innerText = "Fechar pedido"
+    }
 }
