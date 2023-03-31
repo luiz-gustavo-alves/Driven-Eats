d25fd9d9cd4cc889a471e872c4f2ea32f1782414
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-03-30 23:48:25 -0300

    Changed comparison criteria in script
    Using string comparison instead of lenght - criteria modified for automated tests reasons.

--- scripts/general.js
+++ scripts/general.js
@@ -4,12 +4,14 @@ function chooseProduct(selector) {
     let lastChosenProduct = null;
     for (let i = 0; i < selector.parentElement.children.length; i++) {
 
-        if (selector.parentElement.children[i].classList.length > 1) {
+        if (selector.parentElement.children[i].classList[1] == "chosen-product") {
             lastChosenProduct = selector.parentElement.children[i];
             break;
         }
     }
 
-    if (lastChosenProduct != null) lastChosenProduct.classList.remove("chosen-product");
+    if (lastChosenProduct != null) {
+        lastChosenProduct.classList.remove("chosen-product");
+    }
     selector.classList.add("chosen-product");
 }
