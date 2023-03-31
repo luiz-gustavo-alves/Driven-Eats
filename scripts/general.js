3749e5a0847a527554c835105793e82e894753ab
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-03-30 23:45:55 -0300

    Fixed script bug for choosing products.

--- scripts/general.js
+++ scripts/general.js
@@ -1,13 +1,15 @@
 
-
 function chooseProduct(selector) {
+    
+    let lastChosenProduct = null;
+    for (let i = 0; i < selector.parentElement.children.length; i++) {
 
-    const lastChosenProduct = document.querySelector(".chosen-product");
-
-    if (lastChosenProduct != null) {
-        lastChosenProduct.classList.remove("chosen-product");
+        if (selector.parentElement.children[i].classList.length > 1) {
+            lastChosenProduct = selector.parentElement.children[i];
+            break;
+        }
     }
 
-    console.log(selector.classList);
+    if (lastChosenProduct != null) lastChosenProduct.classList.remove("chosen-product");
     selector.classList.add("chosen-product");
 }
